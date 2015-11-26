5d01f2e06ca8cf7a8a615badc0ee107cf63d245a
kieusonlam <lam@toamxinh.com>
2015-11-26 21:28:25 +0700

    Them ngay tao vao KH

--- custom-addons/phaply/models.py
+++ custom-addons/phaply/models.py
@@ -4,11 +4,13 @@ from openerp import models, fields, api
 
 class res_partner_inherit(models.Model):
     _inherit = 'res.partner'
-    
+     
     design = fields.Boolean(string='Thiet ke')
     construct = fields.Boolean(string='Thi cong')
     legal = fields.Boolean(string='Phap ly')
     partner = fields.Many2one(string='Co hoi hop tac', comodel_name='phaply.cohoi')
+    
+    partner_create_date = fields.Date(string='Ngay tao', default=fields.Date.today, readonly=True)
     
     #hoso = fields.Many2many(string="Ho so", comodel_name='phaply2.hoso', relation='hosophaply_rel', column1='hoso_id', column2='name')
     docs = fields.One2many(comodel_name='phaply.hoso', inverse_name='customer')
